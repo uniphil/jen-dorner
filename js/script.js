f60fa42b7a948c7e1c1c025eedc1504c968abf0e
Phil Schleihauf <uniphil@gmail.com>
2014-02-08 13:07:01 -0500

    add a basic thingy with onepage-scroll

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".main").onepage_scroll({
+   sectionContainer: "section",
+   easing: "ease",
+   animationTime: 800,
+   pagination: true,
+   updateURL: false,
+   beforeMove: function(index) {},
+   afterMove: function(index) {},
+   loop: false,
+   keyboard: true,
+   responsiveFallback: false
+});
